bb1846590b6652aef0b93638acb9c691692e3a48
jesusc01 <jesusc01@lintop.localdomain>
2020-03-04 21:22:10 +0100

    just checking the linux implementation

--- lab2.1.1.cpp
+++ lab2.1.1.cpp
@@ -2,7 +2,8 @@
 #include <string>
 
 int main(){
-    std::string consts [15];
+    std::string consts [15]={"m/s^2","predkosc swiatla","dfs","stala plancka","ssds","stala grawitacji","ds","przyspieszenie ziemskie","df","ladunek elementarny"};
     std::cout<<"Tabela stalych fizycznych\n";
+    std::cout<<consts[2]<<std::endl;
     return 0;
 }
