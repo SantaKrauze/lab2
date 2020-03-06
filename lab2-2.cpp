6894f1399b605ec1a83d3e4ccd4e6f2531828de2
SantaKrauze <gracjantokarz38@gmail.com>
2020-03-06 15:52:23 +0100

    drugie dziala

--- lab2-2.cpp
+++ lab2-2.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 
 int main(){
-	int N=0;
-	std::cin>>N;
-	for(int i=1; i<=N; i++){
-		for(int j=1; j<=10; j++){
-			std::cout<<i*j<<std::endl;
-		}	
+
+	int N,M=0;
+	int **tab;
+	std::cout<<"Podaj N i M:\n";
+	std::cin>>N>>M;
+	tab=new int*[N];
+	for (int i = 0; i < N; i++){//alokacja
+		tab[i]=new int[M];
 	}
+
+	for(int i=1; i<=N; i++){//druk i tablicowanie
+		for(int j=1; j<=M; j++){
+			tab[i-1][j-1]=i*j;
+			std::cout<<i<<"*"<<j<<"="<<tab[i-1][j-1]<<" ";
+		}
+		std::cout<<std::endl;	
+	}
+	return 0;
 }
