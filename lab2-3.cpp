22fb0052a3c22c9e2ec7e98c7833364650a1a050
SantaKrauze <gracjantokarz38@gmail.com>
2020-03-09 13:46:29 +0100

    somethings going om

---
+++ lab2-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+	int n=0;
+	std::cin>>n;
+	if(n<1) return 0;
+	int num[n];
+	int **tab;
+	tab=new int*[n];
+	for(int i=0;i<n;i++) num[i]=i+1;//zapelnianie tablicy liczbami 1 do n
+	for(int i=0;i<n;i++) std::cout<<num[i]<<" ";
+	std::cout<<std::endl;
+	for(int i=1;i<n;i++){
+		for(int j=1;j<n;j++) if (i%j==0) tab[i-1][j-1]=j;
+	}	
+	
+	return 0;
+}
