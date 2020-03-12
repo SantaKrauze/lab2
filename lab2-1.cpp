2ba38022262d762d36c18103029e581c73d22e1b
SantaKrauze <gracjantokarz38@gmail.com>
2020-03-12 08:32:58 +0100

    work it harder makeit better do it faster makes us stronger

--- lab2-1.cpp
+++ lab2-1.cpp
@@ -5,25 +5,23 @@ enum class field {X,O,_};
 bool isSpace(field board[3][3]);
 void printBoard(field board[3][3]);
 bool gameOver(field board[3][3]);
-bool circle(field board[3][3]);
-bool cross(field board[3][3]);
+bool fill(int I,field board[3][3]);
 
 int main(){
-    //int k,l;//customowytyp danych typu enum class
     field board[3][3];
     for (int i = 0; i < 3; i++){
             for (int j = 0; j < 3; j++){
                 board[i][j]=field::_;
             }
     }
-
+    field val[2]={field::O,field::X};
     std::cout<<"\nUUUUU KOLKO I KRZYZYK\n";
     printBoard(board);
-    while (gameOver(board)==false){//dodac iteracje pomiedzy funkcjami 
-        if(gameOver(board)==false)circle(board);
-        if(gameOver(board)==false)cross(board);
+    for (int I=0; I!=-1; I++){ 
+        if(gameOver(board)==false)fill(I%2,board);
+	else break;
     }
-    std::cout<<"Koniec gry\n";
+    std::cout<<"\nKoniec gry\n";
     return 0;
 
 }
@@ -31,18 +29,20 @@ void printBoard(field board[3][3]){
     for (int i = 0; i < 3; i++){
         std::cout<<std::endl;
         for (int j = 0; j < 3; j++){
-            if (board[i][j]==field::X) std::cout<<"X ";
-            else if (board[i][j]==field::O) std::cout<<"O ";
-            else std::cout<<"  ";
+            if (board[i][j]==field::X) std::cout<<"|X|";
+            else if (board[i][j]==field::O) std::cout<<"|O|";
+            else std::cout<<"| |";
         }
     }
 }
-bool circle(field board[3][3]){
+bool fill(int I,field board[3][3]){
+    char opt[2]={'X','O'};
+    field val[2]={field::X,field::O};
     int k,l;
-    std::cout<<std::endl<<"Gdzie kolko? Podaj rzad i kolumne:\n";
+    std::cout<<std::endl<<opt[I]<<" Podaj rzad i kolumne:\n";
     std::cin>>k>>l;
     if(k<4&&l<4&&k>0&&l>0&&board[k-1][l-1]==field::_){
-        board[k-1][l-1]=field::O;
+        board[k-1][l-1]=val[I];
         printBoard(board);
     } 
     else {
@@ -51,21 +51,7 @@ bool circle(field board[3][3]){
     }
     return true;
 }
-bool cross(field board[3][3]){
-    int k,l;
-    std::cout<<std::endl<<"Gdzie krzyzyk? Podaj rzad i kolumne:\n";
-    std::cin>>k>>l;
-    
-    if(k<4&&l<4&&k>0&&l>0&&board[k-1][l-1]==field::_){
-        board[k-1][l-1]=field::X;
-        printBoard(board);
-    } 
-    else {
-        std::cout<<"ZLE\n";
-        return false;
-    }
-    return true;
-}
+
 bool gameOver(field board[3][3]){//iteracja
     
     if (isSpace(board)==false) return true;
