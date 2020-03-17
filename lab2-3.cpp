70e983c779b2ca0ee58065742f5f1befc5e42973
SantaKrauze <gracjantokarz38@gmail.com>
2020-03-17 18:12:33 -0400

    it aint much

--- lab2-3.cpp
+++ lab2-3.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
+#include <math.h>
+
+int divisors(int n, int&tab[][]);
+int divQuantity(int n);
 
 int main(){
 	int n=0;
+	std::cout<<"Podaj N:\n";
 	std::cin>>n;
 	if(n<1) return 0;
 	int num[n];
+	for(int i=0; i<n; i++) num[i]=i+1;	
 	int **tab;
-	tab=new int*[n];
-	for(int i=0;i<n;i++) num[i]=i+1;//zapelnianie tablicy liczbami 1 do n
-	for(int i=0;i<n;i++) std::cout<<num[i]<<" ";
-	std::cout<<std::endl;
-	for(int i=1;i<n;i++){
-		for(int j=1;j<n;j++) if (i%j==0) tab[i-1][j-1]=j;//tu sie pojawi dynamika
-	}	
-	
+	tab=new int*[n];	
+	std::cout<<"tab first dim. alloc. ... ok"<<std::endl;
+	for(int i=0; i<n; i++){
+		tab[i]=new int[divQuantity(num[i])];
+		std::cout<<"dla i:"<<i+1<<"="<<divQuantity(num[i])<<std::endl;
+	}
+	delete tab;
 	return 0;
+}
+
+int divisors(int n, int&tab[][]){
+	for (int i=2; i*i<=n; i++){
+		if (n%i==0) tab[n-1][i-1]=i;
+	}
+	return 0;
+}
+
+int divQuantity(int n){
+	int q=1;
+	for (int i=2; i<n; i++){
+		if (n%i==0) q++;
+	}
+	return q+1;
 }
