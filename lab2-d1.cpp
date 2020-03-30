79e0af4dfb1c78b8a2d54a5f5904f8538eb10649
SantaKrauze <gracjantokarz38@gmail.com>
2020-03-30 15:36:43 +0200

    nothing important

--- lab2-d1.cpp
+++ lab2-d1.cpp
@@ -2,28 +2,34 @@
 
 enum class field {red, blue, empty};
 void move(field **board, int n, int m, int player);
+bool gameOver(field**board, int n, int m);
 void print(field **board, int n, int m);
 int main(){
 	field**board;
 	int n,m;
-	std::cout<<"CONNECT FOUR\n"<<"Podaj ilosc rzedow i kolumn planszy\n";
+	std::cout<<"CONNECT FOUR\n"<<"Podaj ilosc kolumn i rzedow planszy\n";
 	std::cin>>n>>m;
 	board=new field*[n];
 	for(int i=0; i<n; i++){
 		board[i]=new field[m];
 	}
 	field players [2]={field::red, field::blue};
-	//print(board, n, m);
+	print(board, n, m);
 	return 0;
 }
 
 void move(field **board, int n, int m, int player){
 	int col;
 	field options[2]={field::red, field::blue};
+	std::cout<<"Podaj kolumne:\n";
 	std::cin>>col;
 	int i=0;
 	while(board[col][i]==field::empty && i<m) i++;
 	board[col][i-1]=options[player];
+}
+
+bool gameOver(field**board, int n, int m){
+	
 }
 
 void print (field **board, int n, int m){
@@ -35,4 +41,4 @@ void print (field **board, int n, int m){
 			else std::cout<<"| |";
 		}
 	}
-}
+}bool gameOver(field**board, int n, int m);
---
+++ lab2-d2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+enum class field {red, blue, empty};
+
+void print(field board [4][4][4]);
+bool gameOver(field board [4][4][4]);
+
+int main(){
+	field board [4][4][4];
+	return 0;
+}
