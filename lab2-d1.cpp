1260813489479f67a4dc3a987a272a8639a4fdb3
SantaKrauze <gracjantokarz38@gmail.com>
2020-03-30 13:10:16 +0200

    initial

---
+++ lab2-d1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+enum class field {red, blue, empty};
+
+void print(field board[n][m]);
+int main(){
+	int n,m;
+	std::cin>>n>>m;
+	field board [n][m];
+	field players [2]={field::red, field::blue};
+	print(board);
+	return 0;
+}
+
+void print (field board[n][m]){
+	for (int i=0; i<n; i++){
+		std::cout<<std::endl;
+		for (int j=0; j<m; j++){
+			if (board[i][j]==field::blue) std::cout<<"|B|";
+			else if (board[n][m]==field::red) std::cout<<"|R|";
+			else std::cout<<"| |";
+		}
+	}
+}
