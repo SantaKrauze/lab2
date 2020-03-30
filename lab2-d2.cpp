1909b9d79af02bfca623ab3f6848bed2548e500c
SantaKrauze <gracjantokarz38@gmail.com>
2020-03-30 22:40:17 +0200

    complete rebuild

--- lab2-d2.cpp
+++ lab2-d2.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
 
 enum class field {red, blue, empty};
-
-void print(field board [4][4][4]);
-bool gameOver(field board [4][4][4]);
-
+class Board{
+	Board board[4][4][4];
+	public:
+		Board();	
+		int getMove();
+		bool gameOver(field board[4][4][4]);
+	private:
+		void _print(Board board);
+		void _move(int i);
+};
 int main(){
-	field board [4][4][4];
+	
+	Board::_print(Board::board);
 	return 0;
+}
+
+void Board::_print(Board board){
+	std::cout<<"Drukowanie planszy od dolu\n";
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			for(int k=0; k<4; k++){
+				//if(Board::board[i][j][k]==field::red) std::cout<<"R";
+				//else if(Board::board[i][j][k]==field::red) std::cout<<"R";
+				//else std::cout<<" ";
+			}
+		}	
+	}
+}
+
+int Board::getMove(){
+	int i;
+	Board::_print(Board::board);
+	std::cin>>i;
+	Board::_move(i);
+	return i;
+}
+
+Board::Board(){
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			//for(int k=0; k<4; k++) Board::board[i][j][k]=field::empty;
+		}	
+	}
+}
+
+void Board::_move(int i){
+	//int i=Board::getMove;
 }
