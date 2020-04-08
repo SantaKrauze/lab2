9b0faf6588b82d53a8d8b1c3bf1e3655c778d823
SantaKrauze <gracjantokarz38@gmail.com>
2020-04-08 20:29:46 +0200

    mostly output changes

--- lab2-2/main.cpp
+++ lab2-2/main.cpp
@@ -1,24 +1,50 @@
 #include <iostream>
 
+void printTable(int**table, int N, int M);
+
 int main(){
 
 	int N,M=0;
-	int **tab;
-	std::cout<<"Podaj N i M:\n";
+	int **table;
+	std::cout<<"Tu powstanie tablica N*M\n"<<"Podaj N i M:\n";
 	std::cin>>N>>M;
-	tab=new int*[N];
+	table=new int*[N];
 	for (int i = 0; i < N; i++){//malloc
-		tab[i]=new int[M];
+		table[i]=new int[M];
 	}
 
 	for(int i=1; i<=N; i++){//saving to array and printing
 		for(int j=1; j<=M; j++){
-			tab[i-1][j-1]=i*j;
-			std::cout<<i<<"*"<<j<<"="<<tab[i-1][j-1]<<" ";
+			table[i-1][j-1]=i*j;
+			//std::cout<<i<<"*"<<j<<"="<<table[i-1][j-1]<<" ";
 		}
 		std::cout<<std::endl;	
 	}
-	for (int i=0; i<N; i++) delete[] tab[i];//freeing memory
-	delete[] tab;
+	printTable(table, N, M);
+	for (int i=0; i<N; i++) delete[] table[i];//freeing memory
+	delete[] table;
 	return 0;
+}
+
+void printTable(int**table, int N, int M){
+	std::cout.width(6);
+	std::cout<<std::right<<"X|";
+	for(int i=1; i<=M; i++){
+		std::cout.width(5);
+		std::cout<<std::right<<i;
+	}
+	std::cout<<std::endl;
+
+	for(int i=0; i<=M; i++) std::cout<<"______";
+	std::cout<<std::endl;
+
+	for(int i=0; i<N; i++){
+		std::cout.width(5);
+		std::cout<<std::right<<i+1<<"|";
+		for(int j=0; j<M; j++){
+			std::cout.width(5);
+			std::cout<<std::right<<table[i][j];
+		}
+		std::cout<<std::endl;
+	}
 }
