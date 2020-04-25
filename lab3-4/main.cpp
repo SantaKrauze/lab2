b98b48dc68e1629c673a2fc80b8fd96a1561bd9d
SantaKrauze <gracjantokarz38@gmail.com>
2020-04-25 20:26:15 +0200

    adding elements now works

--- lab3-4/main.cpp
+++ lab3-4/main.cpp
@@ -20,7 +20,7 @@ class SparseMatrix{
 
 SparseMatrix::SparseMatrix(){
 	std::cout<<"Podaj ilość kolumn i wierszy\n";
-	std::cin>>SparseMatrix::_rows>>SparseMatrix::_columns;
+	std::cin>>_rows>>_columns;
 	std::cout<<"Inicjalizacja macierzy "<<_rows<<"x"<<_columns<<std::endl;
 	std::cout<<SparseMatrix::_valQuantity<<" niezerowych elementow\n";
 }
@@ -39,11 +39,23 @@ void SparseMatrix::addValue(){
 }
 
 void SparseMatrix::addValue(int row, int col, double val){
-	//
+	_matrix[ {row, col} ]=val;
+	std::cout<<row<<"x"<<col<<"="<<val<<std::endl;
+	_valQuantity++;
+	std::cout<<"Ilosc niezerowych elementow: "<<_valQuantity<<std::endl;
 }
 
 void SparseMatrix::printMatrix(){
 	//printing using _getValue()
+	for(int i = 1; i <= _rows; i++){
+		for(int j = 1; j <= + _columns; j++){
+			if (_matrix[ {i, j} ]!=0){
+				std::cout<<_matrix[ {i, j} ]<<"\t";
+			}
+			else std::cout<<"0\t";
+		}
+		std::cout<<std::endl;
+	}
 }
 
 double SparseMatrix::_getValue(int row, int col){
@@ -55,6 +67,8 @@ int main(){
 	std::cout<<"Hello what is this thing\n";
 	SparseMatrix matrix;
 	//switch to adding, printing etc
+	matrix.printMatrix();
 	matrix.addValue();
+	matrix.printMatrix();
 	return 0;
 }
