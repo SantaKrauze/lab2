d93cab599dac59f16e8e8d1ef7883a0dfeee835c
SantaKrauze <gracjantokarz38@gmail.com>
2020-04-25 19:29:34 +0200

    getting smth

--- lab3-4/main.cpp
+++ lab3-4/main.cpp
@@ -1,23 +1,60 @@
 #include <iostream>
 #include <array>
+#include <vector>
+#include <map>
+
 class SparseMatrix{	
 	public:
+		void addValue(int row, int col, double val);
+		void addValue();
+		void printMatrix();
 		SparseMatrix();
-	pirivate:
-		int n,m;
-		std::array < std::array<int,m> , n > _matrix;
-}
+	private:
+		using DOK = std::map< std::array<int,2>, double >;
+		DOK _matrix;
+		int _rows,_columns;
+		int _valQuantity=0;//quantity of non-zero elements/ size of map
+		double _getValue(int row, int col);
+			
+};
 
 SparseMatrix::SparseMatrix(){
 	std::cout<<"Podaj ilość kolumn i wierszy\n";
-	std::cin>>n>>m;
-	for (auto row : _matrix)
-		for (auto val : _matrix )
-			val=0;
+	std::cin>>SparseMatrix::_rows>>SparseMatrix::_columns;
+	std::cout<<"Inicjalizacja macierzy "<<_rows<<"x"<<_columns<<std::endl;
+	std::cout<<SparseMatrix::_valQuantity<<" niezerowych elementow\n";
+}
+
+void SparseMatrix::addValue(){
+	int row,col;
+	double val;
+	std::cout<<"Podaj rzad i kolumne\n";
+	std::cin>>row>>col;
+	std::cout<<"Podaj wartosc\n";
+	std::cin>>val;
+	_matrix[ {row, col} ]=val;
+	std::cout<<row<<"x"<<col<<"="<<val<<std::endl;
+	_valQuantity++;
+	std::cout<<"Ilosc niezerowych elementow: "<<_valQuantity<<std::endl;
+}
+
+void SparseMatrix::addValue(int row, int col, double val){
+	//
+}
+
+void SparseMatrix::printMatrix(){
+	//printing using _getValue()
+}
+
+double SparseMatrix::_getValue(int row, int col){
+	double val = _matrix[ {row,col} ];
+	return val;
 }
 
 int main(){
 	std::cout<<"Hello what is this thing\n";
 	SparseMatrix matrix;
+	//switch to adding, printing etc
+	matrix.addValue();
 	return 0;
 }
