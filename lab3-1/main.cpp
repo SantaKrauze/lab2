de1cd26578195810f8f54407cb3a4b7ee6fd70b2
SantaKrauze <gracjantokarz38@gmail.com>
2020-04-26 18:21:29 +0200

    not much

--- lab3-1/main.cpp
+++ lab3-1/main.cpp
@@ -1,12 +1,27 @@
-#include <iostraem>
+#include <iostream>
 
 class String{
-	const std::string& string;
-	private:
-		const std::string& pattern;
+	public:
+		String();
 		bool compareToPattern();
+
+	private:
+		const std::string& _string;
+		const std::string& _pattern;
+}
+
+String::String(){
+	std::cout<<"Podaj:\n";
+	std::cin>>_string;
+}
+
+bool String::compareToPattern(){
+	
+	return true;//true if ok
 }
 
 int main(){
-	
+	String string;
+	string.compareToPattern;
+	return 0;
 }
