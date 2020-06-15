2f581009892bac353968c41b9ed4e16931945d18
SantaKrauze <gracjantokarz38@gmail.com>
2020-06-15 12:06:19 +0200

    working fine but with map

--- lab4/lab4-4/src/Database.cpp
+++ lab4/lab4-4/src/Database.cpp
@@ -13,4 +13,20 @@ void Database<keyType, dataType>::printByKey(keyType key){
 template <typename keyType, typename dataType>
 void Database<keyType, dataType>::addItem (keyType key, dataType data){
 	data.at(key) = data;
+}
+#include "Database.hpp"
+
+template <typename keyType, typename dataType>
+void Database<keyType, dataType>::printAll(){
+	for (auto i : dataBase) std::cout<<i.first<<'-'<<dataBase.at(i.first)<<'\n';
+}
+
+template <typename keyType, typename dataType>
+void Database<keyType, dataType>::addItem (keyType key, dataType data){
+	dataBase[key] = data;
+}
+
+template <typename keyType, typename dataType>
+dataType Database<keyType, dataType>::getValue (keyType key){
+	return dataBase.at(key);
 }*/
