0ecef9bca528bbd361a0bde524891c5fe9a7042f
SantaKrauze <gracjantokarz38@gmail.com>
2020-06-15 23:14:14 +0200

    done

--- lab4/lab4-4/src/Database.hpp
+++ lab4/lab4-4/src/Database.hpp
@@ -8,28 +8,21 @@ template <typename keyType, typename dataType>
 class Database{
 	public:
 		//Database();
-		Database* left;
-		Database* right;
-		Database* root;
-		void setValue (keyType key, dataType data){
-			dataBase[key] = data;
-		}
-		void printAll(){
-			for (auto i : dataBase) std::cout<<i.first<<'-'<<dataBase.at(i.first)<<'\n';
-		}
-		dataType getValue (keyType key){
-			return dataBase.at(key);
-		}
-		void printByKey (keyType key){
-			for (auto i : dataBase){
-				if (i.first < key){
-					std::cout<<std::endl;
-				}
-				else
-					std::cout<<std::endl;
-			}
-		}
-	private:
-		std::map <keyType, dataType> dataBase;
+		struct node {
+			dataType data;
+			node* left;
+			node* right;
+		};
+		node* root;
+		node* insert();
+		node* remove();
+		void printInOrder();
+		node* find();
+		Database();
+		~Database();
+		void insert();
+		void remove();
+		void display();
+		void search();
 };
 #endif
